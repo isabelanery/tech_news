9d562209a0d92bd7d6827b0998190854dc8f601b
Isabela Nery <isabelanery.r@gmail.com>
2022-11-15 16:30:47 -0300

    feat: search_by_tag

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -27,7 +27,10 @@ def search_by_date(date):
 
 # Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+    return [
+        (news["title"], news["url"])
+        for news in search_news({"tags": {"$regex": tag, "$options": "i"}})
+    ]
 
 
 # Requisito 9
